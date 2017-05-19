552f1a94b03dafef2f2eefe268956777632fb77f
Wesley-4A6B <483509@edu.rocmn.nl>
2017-05-19 15:00:00 +0200

    Testrapportage uitgevoert voor Melvin

--- ProjectP4_test_melvin/database.php
+++ ProjectP4_test_melvin/database.php
@@ -1,5 +1,5 @@
 <?php
-	$dbc = mysqli_connect('localhost', 'root', '', 'over_de_rhein');
+	$dbc = mysqli_connect('localhost', 'root', '', 'over_de_rhein_test_melvin');
 	if(!$dbc) {
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
